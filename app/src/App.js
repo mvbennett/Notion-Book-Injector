6a994974a11d322cea272b20efee365698aaf015
mikebennett <mvbennett9@gmail.com>
2023-01-09 21:34:05 -0500

    connects well to the express api

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
-import search from './myApp';
+// import search from './myApp';
 import Search from './components/Search';
 import Results from './components/Results';
 
 function App() {
+  const search = (book) => {
+    fetch(`http://localhost:4000/search/${book}`)
+    .then(response => response.json())
+    .then(data => console.log(data));
+  }
   const exampleBook = search('pachinko');
   return (
     <div className="App">
--- app/src/myApp.js
+++ app/src/myApp.js
@@ -1,11 +1,11 @@
-import fetch from 'node-fetch';
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config()
+// import fetch from 'node-fetch';
+// import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+// dotenv.config()
 
 // const express = require('express');
 // const app = express();
 // const cors = require('cors');
-// require('dotenv').config();
+require('dotenv').config();
 
 const search = (title) => {
   const result = getBook(title);
