c3e5c5cac1713b0406b984cf3b59514678cb9a55
Michael Bennett <mvbennett9@gmail.com>
2023-01-11 21:01:09 -0500

    added express api into project

---
+++ api/server.js
@@ -0,0 +1,58 @@
+require('dotenv').config();
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+const express = require('express');
+const cors = require('cors');
+const { response } = require('express');
+
+const app = express();
+app.use(cors());
+
+const port = process.env.PORT || 4000;
+
+app.get('/', (req,res) => {
+  res.send({message: "Hello World!"})
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
+
+app.get('/message', (req,res) => {
+  res.send({message: process.env.MESSAGE})
+});
+
+app.get('/search/:book', async (req,res) => {
+  const book = req.params.book;
+  let results = [];
+  await fetch(`https://www.googleapis.com/books/v1/volumes?q=${book}&key=${process.env.BOOK_API}&language=en`)
+  .then(response => response.json())
+  .then((data) => {
+    data.items.forEach(item => {
+      results.push(item.volumeInfo)
+    });;
+  })
+  res.send(results);
+})
+
+app.get('/test/:book', async (req,res) => {
+  const book = req.params.book;
+  let results = [
+    {
+      title: book,
+      author: "Mikey boi",
+      year: 1999
+    },
+    {
+      title: book,
+      author: "Mikey boo",
+      year: 1995
+    },
+    {
+      title: book,
+      author: "Mikey boy",
+      year: 1992
+    }
+  ]
+  res.send(results);
+})
--- test.js
+++
@@ -1,2 +0,0 @@
-const root = document.getElementById('root');
-root.insertAdjacentHTML('afterbegin', '<h1>Hi Mike</h1>');
